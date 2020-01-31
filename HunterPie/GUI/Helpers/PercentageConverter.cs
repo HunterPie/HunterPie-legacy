c0c6e517c603d456af440fa9eff0897c35581397
Haato <matheus_mrp@hotmail.com>
2020-01-30 23:49:28 -0300

    Monster health component now shows percentage

--- HunterPie/GUI/Helpers/PercentageConverter.cs
+++ HunterPie/GUI/Helpers/PercentageConverter.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace HunterPie.GUI {
+namespace HunterPie.GUI.Helpers {
     class PercentageConverter : IMultiValueConverter {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotImplementedException();
+            double ProgressValue = System.Convert.ToDouble(values[0]);
+            double ProgressMaximum = System.Convert.ToDouble(values[1]);
+            return String.Format("{0:0.00}/{1:0.00} ({2:0}%)", ProgressValue, ProgressMaximum, (ProgressValue / ProgressMaximum) * 100);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
