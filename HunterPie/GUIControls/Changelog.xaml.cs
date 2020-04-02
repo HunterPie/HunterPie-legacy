40b3c202d62f7bcd3b15e19fc2648a949c79348e
Haato <matheus_mrp@hotmail.com>
2020-04-02 15:16:20 -0300

    Changelog redesign

--- HunterPie/GUIControls/Changelog.xaml.cs
+++ HunterPie/GUIControls/Changelog.xaml.cs
@@ -1,7 +1,15 @@
-﻿using System.IO;
+﻿using System.Windows.Controls;
 using System;
-using System.Windows.Controls;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
 using System.Windows.Documents;
+using System.Xaml;
+using Markdig.Wpf;
+using XamlReader = System.Windows.Markup.XamlReader;
+using Markdig;
 
 namespace HunterPie.GUIControls {
     /// <summary>
@@ -20,36 +28,40 @@ namespace HunterPie.GUIControls {
 
         public Changelog() {
             InitializeComponent();
-            LoadChangelogText();
+            Loaded += OnLoaded;
         }
 
-        private string GetLineColor(string line) {
-            // TODO: Find a better way for this detection LOL
-            if (line.StartsWith("+") || line.StartsWith("    +")) return "#59FF85";
-            if (line.StartsWith("~") || line.StartsWith("    ~")) return "#FFC13D";
-            if (line.StartsWith("-") || line.StartsWith("    -")) return "#FF6459";
-            return "#FFFFFF";
+        private static MarkdownPipeline BuildPipeline() {
+            return new MarkdownPipelineBuilder()
+                .UseSupportedExtensions()
+                .Build();
         }
 
-        private void LoadChangelogText() {
-            if (!File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "changelog.log"))) {
-                ChangelogBox.AppendText("Changelog not found!");
-                return;
-            }
-            string[] _changelog = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "changelog.log"));
-            foreach (string line in _changelog) {
-                TextRange fLine = new TextRange(ChangelogBox.Document.ContentEnd, ChangelogBox.Document.ContentEnd) {
-                    Text = $"{line}\n"
-                };
-                if (line.StartsWith("UPDATE")) {
-                    fLine.ApplyPropertyValue(TextElement.FontWeightProperty, "Bold");
-                } else {
-                    fLine.ApplyPropertyValue(TextElement.FontWeightProperty, "Light");
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            var markdown = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "changelog.log"));
+            var xaml = Markdig.Wpf.Markdown.ToXaml(markdown, BuildPipeline());
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(xaml))) {
+                using (var reader = new XamlXmlReader(stream, new MyXamlSchemaContext())) {
+                    if (XamlReader.Load(reader) is FlowDocument document) {
+                        Viewer.Document = document;
+                    }
                 }
-                fLine.ApplyPropertyValue(TextElement.ForegroundProperty, GetLineColor(line));
-
             }
         }
 
+        private void OpenHyperlink(object sender, System.Windows.Input.ExecutedRoutedEventArgs e) {
+            Process.Start(e.Parameter.ToString());
+        }
+
+        class MyXamlSchemaContext : XamlSchemaContext {
+            public override bool TryGetCompatibleXamlNamespace(string xamlNamespace, out string compatibleNamespace) {
+                if (xamlNamespace.Equals("clr-namespace:Markdig.Wpf", StringComparison.Ordinal)) {
+                    compatibleNamespace = $"clr-namespace:Markdig.Wpf;assembly={Assembly.GetAssembly(typeof(Markdig.Wpf.Styles)).FullName}";
+                    return true;
+                }
+                return base.TryGetCompatibleXamlNamespace(xamlNamespace, out compatibleNamespace);
+            }
+        }
     }
+
 }
