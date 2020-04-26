7662f96f3d1d23b32b2aaa71e34eab43c4561648
Marcin Śmiałek <msmialek@gmail.com>
2020-04-26 13:59:00 +0200

    Add buffer pools to reduce number of allocations

---
+++ HunterPie/Memory/Buffers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HunterPie.Memory
+{
+    public static class Buffers
+    {
+        private static int capacity;
+        private static Dictionary<Type, object> buffers;
+        private static object lockObject = new object();
+
+        public static void Initialize(int commonCapacity)
+        {
+            capacity = commonCapacity;
+            buffers = new Dictionary<Type, object>();
+        }
+
+        public static void Add<T>(int size = 1) where T : struct
+        {
+            buffers.Add(typeof(T), new BufferPool<T>(capacity, size));
+        }
+
+        public static T[] Get<T>() where T : struct
+        {
+            if (!buffers.ContainsKey(typeof(T)))
+            {
+                lock (lockObject)
+                {
+                    if (!buffers.ContainsKey(typeof(T)))
+                    {
+                        Add<T>();
+                    }
+                }
+            }
+
+            BufferPool<T> pool = (BufferPool<T>) buffers[typeof(T)];
+            return pool.Get();
+        }
+    }
+}
