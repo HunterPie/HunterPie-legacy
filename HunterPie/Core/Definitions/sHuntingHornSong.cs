006092b5df476f1759362348a9a65cc0cccaa57b
Haato3o <matheus_mrp@hotmail.com>
2020-09-04 12:19:19 -0300

    Add song structure

---
+++ HunterPie/Core/Definitions/sHuntingHornSong.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HunterPie.Core.Definitions
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct sHuntingHornSong
+    {
+        public int unkn0;
+        public int unkn1;
+        public int NotesLength; // The amount of notes this song requires
+        public int unkn2; // Always FF FF FF FF?
+        public int Id;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] Notes;
+    }
+}
