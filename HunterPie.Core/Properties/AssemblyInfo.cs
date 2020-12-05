1d802e5762be7a264e56abf9db081d7d9d7323b4
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2020-12-04 23:52:59 -0500

    updated assembly info

--- HunterPie.Core/Properties/AssemblyInfo.cs
+++ HunterPie.Core/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HunterPie.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core library for Hunter Pie and plugins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Haato")]
 [assembly: AssemblyProduct("HunterPie.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
--- HunterPie.UI/Properties/AssemblyInfo.cs
+++ HunterPie.UI/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HunterPie.UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for Hunter Pie UI elements")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Haato")]
 [assembly: AssemblyProduct("HunterPie.UI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
