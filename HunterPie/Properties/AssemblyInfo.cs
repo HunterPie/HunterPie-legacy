f68ffa1ea4f82ab552c2f3cf922afdd937bc9fcb
Haato3o <matheus_mrp@hotmail.com>
2020-12-06 19:59:37 -0300

    bump minor version

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Windows.Media;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.991")]
-[assembly: AssemblyFileVersion("1.0.3.991")]
+[assembly: AssemblyVersion("1.0.3.992")]
+[assembly: AssemblyFileVersion("1.0.3.992")]
 [assembly: NeutralResourcesLanguage("en")]
 
