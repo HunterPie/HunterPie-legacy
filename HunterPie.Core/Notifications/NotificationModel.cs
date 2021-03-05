1bcb2c1a8e30792f499f3bbe0e078d8a34000e39
Haato <35552782+Haato3o@users.noreply.github.com>
2021-03-05 20:06:05 -0300

    Revert "Revert "Notifications widget""

---
+++ HunterPie.Core/Notifications/NotificationModel.cs
@@ -0,0 +1,27 @@
+﻿namespace HunterPie.Notifications
+{
+    public class NotificationModel
+    {
+        public string Header { get; set; }
+        public string Text { get; set; }
+        public int Timeout { get; set; }
+
+        public NotificationModel(string header, string text, int timeout)
+        {
+            Header = header;
+            Text = text;
+            Timeout = timeout;
+        }
+
+        public NotificationModel(string header, string text)
+        {
+            Header = header;
+            Text = text;
+            Timeout = 7000;
+        }
+
+        public NotificationModel()
+        {
+        }
+    }
+}
