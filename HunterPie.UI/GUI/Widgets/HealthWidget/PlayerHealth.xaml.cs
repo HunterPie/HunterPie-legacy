fbad71cbd75589349e2fe68703e0371885ec814e
Haato3o <matheus_mrp@hotmail.com>
2021-04-03 01:01:30 -0300

    Fix: Fixed Health widget not hiding properly in main menu

--- HunterPie.UI/GUI/Widgets/HealthWidget/PlayerHealth.xaml.cs
+++ HunterPie.UI/GUI/Widgets/HealthWidget/PlayerHealth.xaml.cs
@@ -333,7 +333,7 @@ namespace HunterPie.GUI.Widgets.HealthWidget
                     }
                 } else
                 {
-                    WidgetHasContent = true;
+                    WidgetHasContent = false;
                 }
                 ChangeVisibility();
             });
